61d97a15dbf4927840b478843a1cac2db2b07192
bobibul <sunmul27@gmail.com>
2023-08-08 15:52:41 +0900

    카메라 버튼만 구현함 SecondPage로 iso 조절하기

--- lib/main.dart
+++ lib/main.dart
@@ -23,111 +23,103 @@ class Grade extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.amber[800],
       body: Padding(
-          padding: EdgeInsets.fromLTRB(30.0, 130.0, 0.0, 0.0),
+          padding: EdgeInsets.fromLTRB(30.0, 70.0, 30.0, 0.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Center(
                 child: CircleAvatar(
                     backgroundImage:
-                        AssetImage('assets/tooth_outline_icon_139122.png'),
+                    AssetImage('assets/tooth_outline_icon_139122.png'),
                     radius: 60.0,
                     backgroundColor: Colors.amber[800]),
               ),
-              Divider(
-                height: 60.0,
-                color: Colors.grey[850],
-                thickness: 0.5,
-                endIndent: 30.0,
-              ),
-              Text(
-                'NAME',
-                style: TextStyle(color: Colors.white, letterSpacing: 2.0),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Text(
-                'JinHyuk',
-                style: TextStyle(
-                    color: Colors.white,
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Text(
-                '한국어',
-                style: TextStyle(color: Colors.white, letterSpacing: 2.0),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Text(
-                '유진혁',
-                style: TextStyle(
-                    color: Colors.white,
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Row(
-                children: [
-                  Icon(Icons.check_circle_outline),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    'using lightsaver',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      letterSpacing: 1.0,
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(Icons.check_circle_outline),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    'face',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      letterSpacing: 1.0,
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(Icons.check_circle_outline),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    'fire',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      letterSpacing: 1.0,
-                    ),
-                  )
-                ],
-              ),
               Center(
-                child: CircleAvatar(
-                  backgroundImage:
-                      AssetImage('assets/tooth_outline_icon_139122.png'),
-                  radius: 40.0,
-                  backgroundColor: Colors.amber[800],
+                child: Divider(
+                  height: 60.0,
+                  color: Colors.black,
+                  thickness: 10.0,
+                  endIndent: 0.0,
                 ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border.all(color: Colors.black, width: 6)
+                ),
+                child: Text(
+                  '치아 색 분석기',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black,
+                      letterSpacing: 2.0,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+
+
+              SizedBox(
+                height: 60,
+              ),
+
+              Row(
+                children: [
+                  Icon(Icons.check_circle_outline, size: 40,),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    '밝은 환경에서 촬영해주세요',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 1.0,
+                        fontWeight: FontWeight.bold
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.check_circle_outline, size: 40,),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    '5M3 샘플을 준비해주세요',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 1.0,
+                        fontWeight: FontWeight.bold
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 120,
+              ),
+
+              Row(
+                children: [
+                  Text(
+                    '준비완료 시 오른쪽 버튼 클릭',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 1.0,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  FloatingActionButton(onPressed: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => SecondPage()));
+                  },
+                    backgroundColor: Colors.black,
+                    child: Icon(Icons.camera_alt,size: 30),)
+                ],
               )
             ],
           )),
@@ -161,13 +153,13 @@ class MyPage extends StatelessWidget {
             UserAccountsDrawerHeader(
               currentAccountPicture: CircleAvatar(
                 backgroundImage:
-                    AssetImage('assets/tooth_outline_icon_139122.png'),
+                AssetImage('assets/tooth_outline_icon_139122.png'),
                 backgroundColor: Colors.white,
               ),
               otherAccountsPictures: [
                 CircleAvatar(
                   backgroundImage:
-                      AssetImage('assets/tooth_outline_icon_139122.png'),
+                  AssetImage('assets/tooth_outline_icon_139122.png'),
                   backgroundColor: Colors.white,
                 )
               ],
